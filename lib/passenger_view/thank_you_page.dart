ced0224ae485341337d51f67efebb93ee48726a5
aruasa26 <caesar.aruasa@strathmore.edu>
2024-07-15 20:59:52 +0300

    Added a Thank You page

---
+++ lib/passenger_view/thank_you_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class ThankYouPage extends StatelessWidget {
+  const ThankYouPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Payment Successful'),
+        backgroundColor: const Color.fromARGB(255, 108, 105, 105),
+        foregroundColor: Colors.white,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Thank you for using the application!',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Enjoy your ride!',
+                style: TextStyle(fontSize: 20),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 40),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).popUntil((route) => route.isFirst);
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                  textStyle: const TextStyle(fontSize: 18),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  backgroundColor: const Color.fromARGB(255, 108, 105, 105),
+                  foregroundColor: Colors.white,
+                ),
+                child: const Text('Go to Home'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
