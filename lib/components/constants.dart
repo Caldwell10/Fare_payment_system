a20e2859e66a8f5144ebe0ec74f17928103370fa
aruasa26 <caesar.aruasa@strathmore.edu>
2024-07-15 20:53:42 +0300

    Mpesa Credentials  Page

---
+++ lib/components/constants.dart
@@ -0,0 +1,12 @@
+class Constants {
+  static const lipa_na_mpesa_passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2fbf2b6f048ae25b7b4997d8b3f83925';
+  static const business_short_code = '174379';
+  static const consumer_key = 'ZY4WuVa1LXFz2QhAAwFffzl6GHhxNp70D5goRhdxMqoFg6OZ';
+  static const consumer_secret = 'ZHa90s6bfzoG3cNGwld0JQeTad50HQN746VLWcwcbnV0gaMvDtah39GkeCmNgsVJ';
+  static const phone_number = '254742453295';
+  static const transactiontype = 'CustomerPayBillOnline';
+  static const amount = '50';
+  static const callbackURL_sand = 'https://us-central1-fare-payment-system.cloudfunctions.net/mpesaCallback';
+  static const accountref = 'Test123';
+  static const transactionDesc = 'Payment description';
+}
