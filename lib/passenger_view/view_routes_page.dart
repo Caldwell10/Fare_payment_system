561d8854840697f032ec5b9450f9884356626d3b
aruasa26 <caesar.aruasa@strathmore.edu>
2024-07-15 21:02:33 +0300

    Change Url template to openstreetmap

---
+++ lib/passenger_view/view_routes_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:latlong2/latlong.dart';
+
+class ViewRoutesPage extends StatelessWidget {
+  const ViewRoutesPage({super.key});
+
+  Future<List<Polyline>> _fetchRoutes() async {
+    List<Polyline> polylines = [];
+    QuerySnapshot snapshot = await FirebaseFirestore.instance.collection('routes').get();
+
+    for (var doc in snapshot.docs) {
+      List<LatLng> points = (doc['routePoints'] as List)
+          .map((point) => LatLng(point['latitude'], point['longitude']))
+          .toList();
+
+      polylines.add(Polyline(
+        points: points,
+        color: Colors.blue,
+        strokeWidth: 4,
+      ));
+    }
+
+    return polylines;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('View Routes'),
+      ),
+      body: FutureBuilder<List<Polyline>>(
+        future: _fetchRoutes(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error loading routes'));
+          } else {
+            return FlutterMap(
+              options: MapOptions(
+                initialCenter: LatLng(-1.2921, 36.8219),
+                initialZoom: 13,
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  userAgentPackageName: 'com.example.farePaymentSystem',
+                ),
+                PolylineLayer(polylines: snapshot.data!),
+              ],
+            );
+          }
+        },
+      ),
+    );
+  }
+}
