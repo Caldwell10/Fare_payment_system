9a2e7fefa1e880a362572cc766cdfb8e1a17fdc5
aruasa26 <caesar.aruasa@strathmore.edu>
2024-07-15 21:03:26 +0300

    Modified Home Page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,15 @@
 import 'package:fare_payment_system/passenger_view/payment_page.dart';
-import 'package:fare_payment_system/passenger_view/routes_page.dart';
+import 'package:fare_payment_system/passenger_view/view_routes_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../passenger_view/customer_support_page.dart';
 import '../passenger_view/fare_calculation_page.dart';
-import '../passenger_view/select_seat_page.dart';
 
 class HomePage extends StatelessWidget {
-  HomePage({super.key});
+  HomePage({super.key, required this.phoneNumber});
 
+  final String phoneNumber;
   final user = FirebaseAuth.instance.currentUser;
 
   // Sign User Out Method
@@ -19,34 +19,39 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Get the phone number of the logged-in user
+    final phoneNumber = user?.phoneNumber ?? '';
+    print('HomePage initialized with phone number: $phoneNumber');
+
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.grey[45],
-        title: Text('FareFlow',
-        style: TextStyle(
-          fontSize: 35,
-          fontWeight: FontWeight.bold
-        ),
+        backgroundColor: Color.fromARGB(255, 108, 105, 105),
+        title: const Text(
+          'Farely',
+          style: TextStyle(
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
         ),
         actions: [
           IconButton(
             onPressed: signUserOut,
-            icon: Icon(Icons.logout),
+            icon: Icon(Icons.logout, color: Colors.white),
           ),
         ],
       ),
-      body: 
-      GridView.count(
+      body: GridView.count(
         crossAxisCount: 2,
         padding: const EdgeInsets.all(16.0),
         children: [
-          buildGridItem(context, 'Routes', Icons.map, RoutesPage()),
-          buildGridItem(context, 'Select Seat', Icons.event_seat, SelectSeatPage()),
+          buildGridItem(context, 'Routes', Icons.map, ViewRoutesPage()),
           buildGridItem(context, 'Fare Calculation', Icons.calculate, FareCalculationPage()),
           buildGridItem(context, 'Payment', Icons.payment, PaymentPage()),
-          buildGridItem(context, 'Customer Support', Icons.support_agent, CustomerSupportPage()),
+          buildGridItem(context, 'Customer Support', Icons.support_agent, CustomerSupportPage(phoneNumber: phoneNumber)),
         ],
       ),
+      backgroundColor: Colors.grey[200],
     );
   }
 
@@ -59,23 +64,37 @@ class HomePage extends StatelessWidget {
         );
       },
       child: Card(
-        margin: EdgeInsets.all(8.0),
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Icon(icon, size: 50.0),
-              SizedBox(height: 10.0),
-              Text(title, style: TextStyle(fontSize: 16.0)),
-            ],
+        elevation: 5.0,
+        margin: EdgeInsets.all(10.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Container(
+          padding: EdgeInsets.all(20.0),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color.fromARGB(255, 108, 105, 105), Color.fromARGB(255, 108, 105, 105)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(icon, size: 50.0, color: Colors.white),
+                SizedBox(height: 10.0),
+                Text(
+                  title,
+                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold, color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
-
-
-
-
-
